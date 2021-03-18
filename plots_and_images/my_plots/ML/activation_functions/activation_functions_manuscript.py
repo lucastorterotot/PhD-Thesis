43f54127b9176b619df69a826f3a8b8cd28dc40b
Lucas Torterotot <lucas.torterotot@gmail.com>
2021-03-18 18:12:48 +0100

    Script for activation function plots

---
+++ plots_and_images/my_plots/ML/activation_functions/activation_functions_manuscript.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import ltLaTeXpyplot as lt
+
+x = np.arange(-5,5.01,0.01)
+
+class activation_function():
+
+    def __init__(
+        self,
+        x,
+        y,
+        name,
+        label = None,
+        color = 'C0',
+        y_min = None,
+        y_max = None
+    ):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.label = label
+        self.color = color
+        self.plot = lt.ltPlotFct(
+            x,
+            y,
+            label = label,
+            color = color
+        )
+
+        if y_min == None:
+            self.y_min = np.min(y)
+        else:
+            self.y_min = y_min
+
+        if y_max == None:
+            self.y_max = np.max(y)
+        else:
+            self.y_max = y_max
+
+activations = [
+    activation_function(
+        x, x,
+        "linear", label="Linéaire",
+        y_min = -5, y_max = 5
+    ),
+    activation_function(
+        x, (x+abs(x))/2,
+        "relu", label="ReLU",
+        y_min = -1, y_max = 5
+    ),
+    activation_function(
+        x, (x+abs(x))/2 + (np.exp(x)-1)*(1-np.sign(x))/2,
+        "elu", label="ELU",
+        y_min = -2, y_max = 5
+    ),
+    activation_function(
+        x, 1.05070098*(x+abs(x))/2 + 1.05070098*1.67326324*(np.exp(x)-1)*(1-np.sign(x))/2,
+        "selu", label="SELU",
+        y_min = -2, y_max = 5
+    ),
+    activation_function(
+        x, 1/(1+np.exp(-x)),
+        "sigmoid", label="Sigmoïde",
+        y_min = -1, y_max = 2
+    ),
+    activation_function(
+        x, np.tanh(x),
+        "tanh", label="tanh",
+        y_min = -2, y_max = 2
+    ),
+    activation_function(
+        x, np.log(1+np.exp(x)),
+        "softplus", label="Softplus",
+        y_min = -1, y_max = 5
+    ),
+    activation_function(
+        x, x / (1+np.abs(x)),
+        "softsign", label="Softsign",
+        y_min = -2, y_max = 2
+    )
+]
+
+for activation in activations:
+    fig = lt.ltFigure(
+        name = activation.name,
+        height_width_ratio = 1, width_frac = .45
+    )
+    fig.addgraph(
+        'graph',
+        x_label= r"$x$",
+        y_label= r"$f(x)$",
+        show_legend=False,
+        legend_on_side=False,
+        x_min = x.min(), x_max = x.max(),
+        y_min = activation.y_min, y_max = activation.y_max,
+    )
+    fig.fig.axes[-1].axhline(color='C5', linewidth=.5)
+    fig.fig.axes[-1].axvline(color='C5', linewidth=.5)
+    fig.addplot(
+        activation.plot,
+        'graph',
+    )
+
+    fig.save()
+
